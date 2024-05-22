93f85662524ba5dcb09eaa8c745819828b2da9ca
sjoshi29 <sjoshi29@ncsu.edu>
2024-05-22 15:45:27 -0400

    [Implementation] Implemented adding todo task functionality

--- mytodo/src/App.jsx
+++ mytodo/src/App.jsx
@@ -1,26 +1,55 @@
-import TodoList from "./components/TodoList";
-import TodoInput from "./components/TodoInput";
-import { useState } from "react";
-
+import { useState, useEffect } from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
 
-  //for input we use states so that user can input
-  const[todos, setTodos] = useState([
-    "Go to the gym",
-    "Eat more fruits"
-  ])
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+  }
 
-  // we need to update the value of the variable using setTodos
-  function updateTodo (newTodo) {
+  function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
-  
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+
+    console.log(localTodos)
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  }, [])
+
   return (
     <>
-      <TodoInput />
-      <TodoList todos = {todos}/>
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+      <TodoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos} />
     </>
   )
 }
